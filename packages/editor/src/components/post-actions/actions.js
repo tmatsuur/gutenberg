65172d4b2bcd027552d67669596f2aa2003ed3ee
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-06-04 16:30:00 +0200

    Fix incorrect 'delete template' button appearing in plugin-registered templates

--- packages/editor/src/components/post-actions/actions.js
+++ packages/editor/src/components/post-actions/actions.js
@@ -77,7 +77,8 @@ function isTemplateRemovable( template ) {
 	// two props whether is custom or has a theme file.
 	return (
 		template?.source === TEMPLATE_ORIGINS.custom &&
-		! template?.has_theme_file
+		! template?.has_theme_file &&
+		template.origin !== 'plugin'
 	);
 }
 
