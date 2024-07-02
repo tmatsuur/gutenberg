349a75ea3b521cf1c1fdb5e9a9cb6dfd7209b626
Matías Ventura <mv@matiasventura.com>
2024-07-02 12:21:31 +0200

    Data Views: be more clear with the copy of the "hide" action

--- packages/dataviews/src/view-table.tsx
+++ packages/dataviews/src/view-table.tsx
@@ -229,7 +229,7 @@ const _HeaderMenu = forwardRef( function HeaderMenu< Item >(
 						} }
 					>
 						<DropdownMenuItemLabel>
-							{ __( 'Hide' ) }
+							{ __( 'Hide column' ) }
 						</DropdownMenuItemLabel>
 					</DropdownMenuItem>
 				) }
