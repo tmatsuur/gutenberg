6c47cc064d58b434e83271e89575cd98eb45f148
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-07-16 13:47:36 +0200

    Remove unnecessary function_exists() conditional

--- lib/compat/wordpress-6.7/rest-api.php
+++ lib/compat/wordpress-6.7/rest-api.php
@@ -10,22 +10,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Silence is golden.' );
 }
 
-if ( ! function_exists( 'wp_api_template_registry' ) ) {
-	/**
-	 * Hook in to the template and template part post types and modify the rest
-	 * endpoint to include modifications to read templates from the
-	 * BlockTemplatesRegistry.
-	 *
-	 * @param array  $args Current registered post type args.
-	 * @param string $post_type Name of post type.
-	 *
-	 * @return array
-	 */
-	function wp_api_template_registry( $args, $post_type ) {
-		if ( 'wp_template' === $post_type || 'wp_template_part' === $post_type ) {
-			$args['rest_controller_class'] = 'Gutenberg_REST_Templates_Controller_6_7';
-		}
-		return $args;
+/**
+ * Hook in to the template and template part post types and modify the rest
+ * endpoint to include modifications to read templates from the
+ * BlockTemplatesRegistry.
+ *
+ * @param array  $args Current registered post type args.
+ * @param string $post_type Name of post type.
+ *
+ * @return array
+ */
+function wp_api_template_registry( $args, $post_type ) {
+	if ( 'wp_template' === $post_type || 'wp_template_part' === $post_type ) {
+		$args['rest_controller_class'] = 'Gutenberg_REST_Templates_Controller_6_7';
 	}
+	return $args;
 }
 add_filter( 'register_post_type_args', 'wp_api_template_registry', 10, 2 );
